2736908c0c5474d83618cd4c2cfc340bcc403c5e
Shabaz K Mullalkar <smullalkar@gmail.com>
2020-09-06 00:04:06 +0530

    Modal of registeration added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Navbar.css'
+import Registeration from '../Registeration/Registeration'
 // #bb392a
 // #222;
 export default function Navbar() {
@@ -10,7 +11,22 @@ export default function Navbar() {
                 <form className="form-inline">
                     <img alt='...' className='mr-3' src='/home.svg' style={{ width: '20px', height: '20px' }} />
                     <div className="btn my-2 text-secondary my-sm-0">LOGIN</div>
-                    <div className="btn my-2 text-secondary my-sm-0">REGISTER</div>
+                    <div className="btn my-2 text-secondary my-sm-0" data-toggle="modal" data-target="#exampleModalLong">REGISTER</div>
+                    <div className="modal fade" id="exampleModalLong" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+                        <div className="modal-dialog modal-lg" role="document">
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <h5 className="modal-title" id="exampleModalLongTitle">Registeration</h5>
+                                    <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div className="modal-body">
+                                    <Registeration />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                     <div className="btn my-2 text-secondary my-sm-0 mr-5">CONTACT US</div>
                 </form>
             </nav>
