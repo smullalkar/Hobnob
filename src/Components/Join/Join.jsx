92af6d9ac473b1abb13023584b46c902da4907f0
Shabaz K Mullalkar <smullalkar@gmail.com>
2020-09-05 18:36:14 +0530

    Join or signup component done

---
+++ src/Components/Join/Join.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Join() {
+    return (
+        <div className='text-center px-5 pt-5 m-5'>
+            <h1 className='font-weight-bold text-secondary p-2'>Join The Community</h1>
+            <h4 className='font-italic'>Know Your Policy | Cashless Assistance | Claim Reimbursements </h4>
+            <div className='d-flex justify-content-center p-2'>
+                <button className='btn m-2 text-light' style={{backgroundColor:'#bb392a'}}>REGISTER NOW</button>
+                <button className='btn m-2 text-light' style={{backgroundColor:'#bb392a'}}>LOGIN</button>
+                <button className='btn m-2 text-light' style={{backgroundColor:'#bb392a'}}>CONTACT US</button>
+            </div>
+        </div>
+    )
+}
