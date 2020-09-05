bd276ef30450f908992d35ec2a93e0ff244feeea
Shabaz K Mullalkar <smullalkar@gmail.com>
2020-09-05 16:39:02 +0530

    Small changes done

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
-
+import './Navbar.css'
+// #bb392a
+// #222;
 export default function Navbar() {
     return (
         <div>
-            <nav class="navbar navbar-light bg-light justify-content-between">
-                <img src='/hobnob.svg' class="navbar-brand" style={{ width: '100px', height: '50px' }} />
-                <form class="form-inline">
-                    <button class="btn btn-outline-success my-2 my-sm-0">Login</button>
+            <nav className="navbar navbar-light justify-content-between">
+                <img alt='...' src='/hobnob.svg' className="navbar-brand ml-5" style={{ width: '100px', height: '50px' }} />
+                <form className="form-inline">
+                    <img alt='...' className='mr-3' src='/home.svg' style={{ width: '20px', height: '20px' }} />
+                    <div className="btn my-2 text-secondary my-sm-0">LOGIN</div>
+                    <div className="btn my-2 text-secondary my-sm-0">REGISTER</div>
+                    <div className="btn my-2 text-secondary my-sm-0 mr-5">CONTACT US</div>
                 </form>
             </nav>
         </div>
