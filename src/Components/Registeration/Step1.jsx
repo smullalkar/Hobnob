84499a5d9edd9341964d989db010f02851504b8e
Shabaz K Mullalkar <smullalkar@gmail.com>
2020-09-06 17:16:13 +0530

    step 1 of resteration

--- src/Components/Registeration/Step1.jsx
+++ src/Components/Registeration/Step1.jsx
@@ -10,28 +10,40 @@ export class Step1 extends Component {
         const { values, inputChange } = this.props;
 
         return (
-            <div className="form-container">
-                <div className="form-group">
-                    <label htmlFor="name">Name</label>
+            <div className="form-container text-left">
+                <div className="input-group mb-2 mr-sm-2 col-7 offset-2 my-3">
+                    <label className="col-sm-4 col-form-label" htmlFor="name">Name</label>
+                    <div className="input-group-prepend">
+                        <div className="input-group-text"><img alt='...' src='/user.svg' style={{ width: '15px', height: '15px' }} /></div>
+                    </div>
                     <input type="text" className="form-control" name="name" onChange={inputChange} value={values.name} />
                 </div>
-                <div className="form-group">
-                    <label htmlFor="email">Email</label>
+                <div className="input-group mb-2 mr-sm-2 col-7 offset-2 my-3">
+                    <label className="col-sm-4 col-form-label" htmlFor="email">Email</label>
+                    <div className="input-group-prepend">
+                        <div className="input-group-text"><img alt='...' src='/email.svg' style={{ width: '15px', height: '15px' }} /></div>
+                    </div>
                     <input type="email" className="form-control" name="email" onChange={inputChange} value={values.email} />
                 </div>
-                <div className="form-group">
-                    <label htmlFor="phone">Phone Number</label>
-                    <input type="number" className="form-control" name="phone" onChange={inputChange} value={values.phone} />
+                <div className="input-group mb-2 mr-sm-2 col-7 offset-2 my-3">
+                    <label className="col-sm-4 col-form-label" htmlFor="phone">Phone</label>
+                    <div className="input-group-prepend">
+                        <div className="input-group-text"><img alt='...' src='/phone.svg' style={{ width: '15px', height: '15px' }} /></div>
+                    </div>
+                    <input type="text" className="form-control" name="phone" onChange={inputChange} value={values.phone} />
                 </div>
-                <div className="form-group">
-                    <label htmlFor="password">Password</label>
+                <div className="input-group mb-2 mr-sm-2 col-7 offset-2 my-3">
+                    <label className="col-sm-4 col-form-label" htmlFor="password">Password</label>
+                    <div className="input-group-prepend">
+                        <div className="input-group-text"><img alt='...' src='/password.svg' style={{ width: '15px', height: '15px' }} /></div>
+                    </div>
                     <input type="password" className="form-control" name="password" onChange={inputChange} value={values.password} />
                 </div>
 
                 <br />
 
                 <div className="text-right">
-                    <button className="btn btn-primary" onClick={this.continue}>Continue</button>
+                    <button className='btn text-light' style={{ backgroundColor: '#bb392a' }} onClick={this.continue}>Continue</button>
                 </div>
             </div>
         )
