e9cef90c485260f15a5ae41c5bc577725cdb7b9e
Shabaz K Mullalkar <smullalkar@gmail.com>
2020-09-06 17:15:49 +0530

    calling api on pincode

--- src/Components/Registeration/Registeration.jsx
+++ src/Components/Registeration/Registeration.jsx
@@ -4,6 +4,7 @@ import Step2 from './Step2';
 import Step3 from './Step3';
 import Confirm from './Confirm';
 import Success from './Success';
+import axios from 'axios'
 
 export class Form extends Component {
     state = {
@@ -16,7 +17,14 @@ export class Form extends Component {
         dob: new Date(),
         city: '',
         stat: '',
-        pincode: ''
+        pincode: '',
+        cityAvailable: false,
+        spouse: false,
+        father: false,
+        mother: false,
+        child1: false,
+        child2: false,
+        
     };
 
     nextStep = () => {
@@ -28,6 +36,40 @@ export class Form extends Component {
         const { step } = this.state;
         this.setState({ step: step - 1 });
     };
+
+    search = () => {
+        if (this.state.pincode !== '') {
+            return axios
+                .get(`https://pincode.saratchandra.in/api/pincode/${this.state.pincode}`)
+                .then(res => {
+                    if (res.status === 200) {
+                        this.setState({
+                            cityAvailable: true,
+                            city: res.data.data[0].district,
+                            stat: res.data.data[0].state_name
+
+                        })
+                    }
+                    // else if (res.status === 404) {
+                    //     this.setState({ cityAvailable: false })
+                    // }
+                })
+                .catch(err => this.setState({
+                    cityAvailable: false,
+                    city: '',
+                    stat: ''
+                }));
+        }
+        else {
+            return
+        }
+    }
+
+    onType = (e) => {
+        this.setState({ pincode: e.target.value });
+        clearTimeout(this.debouncedType);
+        this.debouncedType = setTimeout(this.search, 500)
+    }
 
     inputChange = (e) => {
         this.setState({
@@ -43,9 +85,9 @@ export class Form extends Component {
 
     render() {
         const { step } = this.state;
-        const { name, email, phone, password, gender, dob, city, stat, pincode } = this.state;
-        const values = { name, email, phone, password, gender, dob, city, stat, pincode };
-        console.log('state',this.state)
+        const { name, email, phone, password, gender, dob, city, stat, pincode, cityAvailable } = this.state;
+        const values = { name, email, phone, password, gender, dob, city, stat, pincode, cityAvailable };
+        console.log('state', this.state)
         switch (step) {
             case 1:
                 return (
@@ -61,8 +103,9 @@ export class Form extends Component {
                         nextStep={this.nextStep}
                         prevStep={this.prevStep}
                         inputChange={this.inputChange}
+                        onType={this.onType}
                         handleDob={this.handleDob}
-                        date = {dob}
+                        date={dob}
                         values={values}
                     />
                 );
